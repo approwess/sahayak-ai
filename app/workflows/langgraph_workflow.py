2be7828d8a5906ebd64c4f079ce2293ad5da31d3
Ajinkya Bandagale <ajinkya.bandagale@gmail.com>
2025-07-27 11:27:13 +0530

    workflow updated

--- app/workflows/langgraph_workflow.py
+++ app/workflows/langgraph_workflow.py
@@ -9,8 +9,6 @@ from app.services.lesson_generator import (
 )
 from app.services.visual_workflow_nodes import (
     generate_resources,
-    generate_content,
-    process_lesson_plan,
     generate_visual_content
 )
 
@@ -21,14 +19,8 @@ def create_workflow():
     graph.add_node("classifier", determine_class_type)
     graph.add_node("single_professor", generate_single_grade_lesson)
     graph.add_node("multigrade_professor", generate_multigrade_lesson)
-    
-    # New visual generation nodes
-    # graph.add_node("extract_requirements", extract_visual_requirements)
     graph.add_node("generate_visuals", generate_visual_content)
-
     graph.add_node("generate_resources", generate_resources)
-    #graph.add_node("generate_content", generate_content)
-    #graph.add_node("process_lesson_plan", process_lesson_plan)
     
     # Set entry point
     graph.set_entry_point("classifier")
@@ -42,34 +34,10 @@ def create_workflow():
             "multigrade_professor": "multigrade_professor"
         }
     )
-    
-    # Updated conditional routing for visual generation - use "__end__" instead of "END"
-    # graph.add_conditional_edges(
-    #     "single_professor",
-    #     should_generate_visuals,
-    #     {
-    #         "generate_visuals": "extract_requirements",
-    #         "END": "__end__"  # Changed from "END" to "__end__"
-    #     }
-    # )
-    
-    # graph.add_conditional_edges(
-    #     "multigrade_professor", 
-    #     should_generate_visuals,
-    #     {
-    #         "generate_visuals": "extract_requirements",
-    #         "END": "__end__"  # Changed from "END" to "__end__"
-    #     }
-    # )
 
     graph.add_edge("single_professor", "generate_resources")
     graph.add_edge("multigrade_professor", "generate_resources")
     graph.add_edge("generate_resources", "generate_visuals")
-    
-    # Visual workflow edges
-    #graph.add_edge("generate_resources", "generate_content")
-    #graph.add_edge("generate_content", "process_lesson_plan")
-    #graph.add_edge("process_lesson_plan", END)  # This connects to the END node
     graph.add_edge("generate_visuals", END)  # This connects to the END node
     
     return graph.compile()
